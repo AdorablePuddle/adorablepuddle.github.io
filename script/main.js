f946e91cbfdb82218e9cec080454062c31681ef9
UselessAmber <mainrurikoproject@gmail.com>
2025-05-21 16:11:59 +1000

    torned

--- script/main.js
+++ script/main.js
@@ -8,6 +8,13 @@ function validation(id){
         if (a.toLowerCase() === "ribbon"){
             alert("You found her.")
             open("https://docs.google.com/document/d/1rDYz8kdn3ajeU3WpqHYIMUwrum_gYx_Yu1BOqThzJwE/edit?usp=sharing")
+        } else if (a.toLowerCase() === "torned ribbon") {
+            alert("In a different world, she lied to them all.")
+            alert("She kept a seed to herself, leaving her weak and frail.")
+            alert("That seed of hope empowers the people she loved.")
+            alert("And in return")
+            alert("it made her a monster.")
+            open("https://docs.google.com/document/d/1N1Jc9ahisq7pOaOVCPrM-DzRoD7GTHutYBGx-4kdYs8/edit?usp=sharing")
         }else{
             alert("Incorrect Solution.")
         }
